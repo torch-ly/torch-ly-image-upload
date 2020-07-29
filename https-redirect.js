5ac93f169f42ad900a11449b76089361718d04d9
EricHier <developer.erichier@gmail.com>
2020-07-29 19:55:36 +0200

    fixed redirect url

--- https-redirect.js
+++ https-redirect.js
@@ -3,7 +3,7 @@ import express from "express";
 export function startRedirectServer() {
     let http = express();
 
-    http.get('*', (req, res) => res.redirect('https://server.erichier.tech' + req.headers.host + req.url));
+    http.get('*', (req, res) => res.redirect('https://server.erichier.tech' + req.url));
 
     http.listen(process.env.PORT);
 }
